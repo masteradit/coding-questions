9cbaa3696fc179f425245b22735e014b6efe3a54
Master Adit <aditluhadia@gmail.com>
2020-09-25 14:44:19 +0530

    Create solve-me-first.cpp

---
+++ C++/HackerRank/Algorithms/solve-me-first.cpp
@@ -0,0 +1,20 @@
+// Solve Me First
+// https://www.hackerrank.com/challenges/solve-me-first/problem
+
+#include <iostream>
+using namespace std;
+
+int solveMeFirst(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int num1, num2;
+    int sum;
+    cin >> num1 >> num2;
+    sum = solveMeFirst(num1, num2);
+    cout << sum;
+    return 0;
+}
