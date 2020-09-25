70efa83743e0638c6d51a9af627f7d0f8690be18
Master Adit <aditluhadia@gmail.com>
2020-09-25 18:29:24 +0530

    Create cpp-hello-world.cpp

---
+++ C++/HackerRank/Practice/cpp-hello-world.cpp
@@ -0,0 +1,11 @@
+// Say "Hello, World!" With C++
+// https://www.hackerrank.com/challenges/cpp-hello-world/problem
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!";
+    return 0;
+}
