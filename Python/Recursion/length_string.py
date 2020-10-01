d01f9118a107e75130785f9a9f07fcfe946597b5
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-10-02 02:31:29 +0530

    Create length_string.py
    Python program to find length of string using recursion

---
+++ Python/Recursion/length_string.py
@@ -0,0 +1,10 @@
+#Python program to find length of string using recursion
+
+def string_length(str) : 
+    if str == '': 
+        return 0
+    else : 
+        return 1 + string_length(str[1:])  
+      
+str = input("Enter the string: ")
+print (string_length(str))
