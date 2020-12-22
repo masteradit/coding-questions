0bc6361db95ee0be421ec5559df8529fb8ea8932
Adit Luhadia <39951503+masteradit@users.noreply.github.com>
2020-12-23 00:24:17 +0530

    Update Prime check.cpp

--- C++/Basics/Prime check.cpp
+++ C++/Basics/Prime check.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
     cin >> n;
     bool prime = true;
-    
-    for (int i = 2; i < n / 2; i++) {
-        if (n % i == 0) {
-            prime = false;
-            break;
-        }
+    if (n <= 1)
+    {
+        cout << "Neither prime nor composite";
     }
-    
-    if (prime) {
-        cout << "Prime";
-    } else {
-        cout << "Not prime";
+    else
+    {
+        for (int i = 2; i < n / 2; i++)
+        {
+            if (n % i == 0)
+            {
+                prime = false;
+                break;
+            }
+        }
+
+        if (prime)
+        {
+            cout << "Prime";
+        }
+        else
+        {
+            cout << "Not prime";
+        }
     }
 }
